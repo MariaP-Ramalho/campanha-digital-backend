a3494a627293e81d4a47dc06843f3ab25767f188
MariaP-Ramalho <eduardapramalho@gmail.com>
2025-05-14 15:52:21 -0300

    initial commit

---
+++ src/main/java/br/com/wtd/analisedelive/AnaliseDeLiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.wtd.analisedelive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseDeLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnaliseDeLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wtd/analisedelive/AnaliseDeLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wtd.analisedelive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnaliseDeLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
