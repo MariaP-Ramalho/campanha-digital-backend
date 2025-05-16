c0785c83065bb14a13660285fe3fae351036fa2b
MariaP-Ramalho <eduardapramalho@gmail.com>
2025-05-16 15:04:10 -0300

    refactor: main organization

--- src/main/java/br/com/wtd/analisedelive/main/Main.java
+++ src/main/java/br/com/wtd/analisedelive/main/Main.java
@@ -5,32 +5,20 @@ import br.com.wtd.analisedelive.model.CommentsInfoData;
 import br.com.wtd.analisedelive.model.GeneralInfoData;
 import br.com.wtd.analisedelive.repository.CommentRespository;
 import br.com.wtd.analisedelive.service.CheckLiveActivity;
-import br.com.wtd.analisedelive.service.ConsumeApi;
 import br.com.wtd.analisedelive.service.ConvertData;
 import br.com.wtd.analisedelive.service.FetchLiveComments;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import io.github.cdimascio.dotenv.Dotenv;
-import okhttp3.HttpUrl;
-
-import java.sql.SQLOutput;
-import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     private final CheckLiveActivity checkLive = new CheckLiveActivity();
     private final FetchLiveComments fetchLiveComments = new FetchLiveComments();
-    private final Dotenv dotenv = Dotenv.load();
-    private final String VIDEO_ID = dotenv.get("VIDEO_ID");
-    private final String YOUTUBE_API_KEY = dotenv.get("YOUTUBE_API_KEY");
-    private final String SHEET_ID = dotenv.get("SHEET_ID");
     private final CommentRespository repository;
-    private final ConsumeApi consume = new ConsumeApi();
     private final ConvertData converter = new ConvertData();
 
     public Main(CommentRespository repository) {
         this.repository = repository;
     }
-
 
     public void runMain() throws JsonProcessingException {
 
@@ -55,10 +43,8 @@ public class Main {
     }
 
     public void processComments(GeneralInfoData generalInfoData) {
-        System.out.println("Entrou no processamento");
-        System.out.println("Comentários recebidos: " + generalInfoData.commentsInfo());
         for(CommentsInfoData commentsInfoData : generalInfoData.commentsInfo()){
-            System.out.println("entrou no for");
+
             CommentsInfo commentsInfo = new CommentsInfo(
                     commentsInfoData.commentId(),
                     commentsInfoData.commentsDetail(),
